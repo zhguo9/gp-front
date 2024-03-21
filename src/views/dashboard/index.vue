9e090da183ed5af06a12e5843cca0dc7172e8c82
zhguo9 <zhguo9@gmail.com>
2024-03-21 16:04:27 +0800

    上传文件代码完成

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <el-upload
+      class="upload-demo"
+      action="http://localhost:8080/api/upload"
+      :on-success="handleSuccess"
+      :before-upload="beforeUpload"
+      drag
+      multiple>
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+    </el-upload>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleSuccess(response, file, fileList) {
+      this.$message({
+        message: '文件上传成功',
+        type: 'success'
+      });
+    },
+    beforeUpload(file) {
+      // 在这里可以进行文件类型和大小的校验
+      return true;
+    }
+  }
+}
+</script>
+
+<style>
+.upload-demo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  border: 1px dashed #409EFF;
+  border-radius: 6px;
+  background-color: #f0f0f0;
+  color: #909399;
+  cursor: pointer;
+}
+</style>
